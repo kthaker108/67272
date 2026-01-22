e27a3bde6780737eb6f406dc4cdbbe89d680bb75
Khushi <kthaker@andrew.cmu.edu>
2026-01-22 11:58:45 -0500

    adding to branch

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,10 +1,18 @@
-def convert(temp)
+def convert(temp, measure = "F")
     return "Temperature must be an integer" unless temp.class == Integer
-    return "Temperature below Absolute Zero" if temp < -474
-    5 * (temp - 32)/9
+    return "Temperature below Absolute Zero" if below_absolute_zero?(temp, measure)
 
-
+    if (measure == "F")
+        5 * (temp - 32)/9
+    else 
+        (temp * 9/5) + 32
+    end
 end
+
+def below_absolute_zero?(temp, measure)
+    (temp < -454 and measure == "F") or (temp < -270 and measure == "C")
+end
+
 
 # TESTS
 puts "Converting Fahrenheit to Celsius"
@@ -13,3 +21,9 @@ puts convert(50)
 puts convert(212)
 puts convert("zero")
 puts convert(-500)
+
+puts "Converting Celsius to Fahrenheit"
+puts convert(0, "C")
+puts convert(10, "C")
+puts convert(100, "C")
+puts convert(-280, "C")

c96a16558a67bdafa4d6a994a4997ea0f4184412
Khushi <kthaker@andrew.cmu.edu>
2026-01-22 11:46:17 -0500

    revisions

---
+++ temp_conversions.rb
@@ -0,0 +1,13 @@
+def convert(temp)
+    return "Temperature must be an integer" unless temp.class == Integer
+    5 * (temp - 32)/9
+
+
+end
+
+# TESTS
+puts "Converting Fahrenheit to Celsius"
+puts convert(32)
+puts convert(50)
+puts convert(212)
+puts convert("zero")
